fd410b842952f0bb58fb5405362bdc01caed7b7c
Artur 'Wodor' Wielogorski <wodor@wodor.net>
2018-12-06 10:27:12 +0000

    Init

---
+++ stopInfo.js
@@ -0,0 +1,7 @@
+var fs = require('fs');
+
+// var contents = JSON(fs.readFileSync('data/metrolink_response.json', 'utf8')).parse();
+var data = require('./data/metrolink_response.json');
+console.log(data);
+
+
