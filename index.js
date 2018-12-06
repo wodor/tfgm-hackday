d366518a4c89547f5da6639653bad31b009bf1f3
Artur 'Wodor' Wielogorski <wodor@wodor.net>
2018-12-06 14:51:43 +0000

    index

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const api = require ('./api.js')
 exports.handler = async (event) => {
 
     let result = await api.callMetrolinkApi('6d674edb6dc3405fb72a0d21f9de9680')
-    .then(response => api.getDeparturesData("Market Street", response))
+    .then(response => api.getDeparturesData(event.stop, response))
+.then(result => api.formatOutput(result))
     // TODO implement
     const response = {
         statusCode: 200,
